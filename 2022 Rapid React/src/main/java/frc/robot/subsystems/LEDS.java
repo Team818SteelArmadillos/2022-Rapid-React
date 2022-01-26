6cc16d0c0692c20ee56e68979eb5f6143a68dda5
Matthew White <56370616+Mj2758@users.noreply.github.com>
2022-01-26 18:41:58 -0500

    Update LEDS.java

--- 2022 Rapid React/src/main/java/frc/robot/subsystems/LEDS.java
+++ 2022 Rapid React/src/main/java/frc/robot/subsystems/LEDS.java
@@ -15,25 +15,32 @@ import edu.wpi.first.wpilibj.util.Color;
 public class LEDS extends SubsystemBase {
 
     CANdle candle;
-    Color ballColor;
-    ColorSensorV3 colorSensor;
+    Color ballColor1;
+    Color ballColor2;
+    ColorSensorV3 colorSensor1;
+    ColorSensorV3 colorSensor2;
     I2C.Port i2cPort;
     ColorMatch m_colorMatcher = new ColorMatch();
 
     public LEDS() {
         i2cPort = I2C.Port.kOnboard;
-        ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
-        ballColor = colorSensor.getColor();
-        SmartDashboard.putNumber("Red", ballColor.red);
-        SmartDashboard.putNumber("Blue", ballColor.blue);
+        ColorSensorV3 colorSensor1 = new ColorSensorV3(i2cPort);
+        ballColor1 = colorSensor1.getColor();
+        ColorSensorV3 colorSensor2 = new ColorSensorV3(i2cPort);
+        ballColor2 = colorSensor2.getColor();
+        // supposed to report to smart dashboard the color of each of ball will say blue and or red depeding on ball color
+        SmartDashboard.putNumber("Red", ballColor1.red);
+        SmartDashboard.putNumber("Red", ballColor2.red);
+        SmartDashboard.putNumber("Blue", ballColor1.blue);
+        SmartDashboard.putNumber("Blue", ballColor2.blue);
 
         candle = new CANdle(0);
         CANdleConfiguration config = new CANdleConfiguration();
         config.stripType = LEDStripType.RGB;
         candle.configAllSettings(config);
 
-        //sets the leds on the canfle itself to white
-      //  candle.setLEDs(0, 0, 0, 255, 0, LEDs_Full);
+        //sets the leds on the canfle itself to lime green
+       candle.setLEDs(50, 205, 50, 255, 0, LED_CANdle);
 
 }
 }
