344c1f3e14e41058b1e560c0afcba46ab4c4569d
Jackstevenson13 <59897917+Jackstevenson13@users.noreply.github.com>
2022-01-28 20:07:43 -0500

    Color Update

--- 2022 Rapid React/src/main/java/frc/robot/subsystems/IndexSubsystem.java
+++ 2022 Rapid React/src/main/java/frc/robot/subsystems/IndexSubsystem.java
@@ -3,9 +3,12 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.motorports.*;
 import static frc.robot.Constants.sensorPorts.*;
@@ -15,8 +18,15 @@ public class IndexSubsystem extends SubsystemBase {
   private DigitalInput SensorFront, SensorBack;
   private TalonFX indexMotor;
   private VictorSPX conveyorMotor;
+  ColorSensorV3 colorSensor1;
+  ColorSensorV3 colorSensor2;
+  I2C.Port i2cPort;
 
   public IndexSubsystem() {
+
+    i2cPort  = I2C.Port.kOnboard;
+    colorSensor1 = new ColorSensorV3(i2cPort);
+
 
     indexMotor = new TalonFX(indexMotorPortUpper);
     indexMotor.configFactoryDefault();
@@ -25,6 +35,10 @@ public class IndexSubsystem extends SubsystemBase {
     SensorFront = new DigitalInput(indexSensorFront);
     SensorBack = new DigitalInput(indexSensorBack);
 
+  }
+
+  public Color getBallColor() {
+    return colorSensor1.getColor();
   }
 
   public void doIndex(double Speed) {
