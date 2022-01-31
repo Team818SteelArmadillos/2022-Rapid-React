07cbe681dd52d2e7a93582f6db45d0f3e1d6783b
Jackstevenson13 <59897917+Jackstevenson13@users.noreply.github.com>
2022-01-31 17:45:18 -0500

    Update IntakeCommand.java

--- 2022 Rapid React/src/main/java/frc/robot/commands/IntakeCommand.java
+++ 2022 Rapid React/src/main/java/frc/robot/commands/IntakeCommand.java
@@ -3,22 +3,18 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-import frc.robot.Robot;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class IntakeCommand extends CommandBase {
   /** Creates a new IntakeCommand. */
-
   public IntakeCommand() {
-    addRequirements(Robot.m_IntakeSubsystem);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-  
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
