8c5cb7566528c29c7907ca1b24ee3db489bd3b77
JaxonAn23 <59897973+JaxonAn23@users.noreply.github.com>
2022-02-01 20:09:21 -0500

    Made the backwards shooter command

--- 2022 Rapid React/src/main/java/frc/robot/commands/BackwardsTurretCommand.java
+++ 2022 Rapid React/src/main/java/frc/robot/commands/BackwardsTurretCommand.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
 
 public class BackwardsTurretCommand extends CommandBase {
   /** Creates a new BackwardsTurretCommand. */
@@ -14,15 +15,23 @@ public class BackwardsTurretCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    Robot.m_ElevatorSubsystem.setDynamicPistons(1);
+    Robot.m_TurretSubsystem.setTurretSpeed(0);
+    Robot.m_TurretSubsystem.resetEncoderPosition();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    Robot.m_TurretSubsystem.setTurretSpeed(Robot.m_oi.getleftXAxis());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    Robot.m_TurretSubsystem.setTurretSpeed(0);
+  }
 
   // Returns true when the command should end.
   @Override
