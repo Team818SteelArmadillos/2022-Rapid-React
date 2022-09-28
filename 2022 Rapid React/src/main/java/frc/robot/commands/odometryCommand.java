0bc24ccec154ddeff3d217b2b223f3d36d65a2a2
Noah Tonn <843557@wcskids.net>
2022-09-28 19:51:22 -0400

    ASoghadsf;ohas

--- 2022 Rapid React/src/main/java/frc/robot/commands/odometryCommand.java
+++ 2022 Rapid React/src/main/java/frc/robot/commands/odometryCommand.java
@@ -4,5 +4,29 @@
 
 package frc.robot.commands;
 
-/** Add your docs here. */
-public class odometryCommand {}
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class OdometryCommand extends CommandBase {
+  /** Creates a new OdometryCommand. */
+  public OdometryCommand() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- 2022 Rapid React/src/main/java/frc/robot/subsystems/OdometrySubsystem.java
+++ 2022 Rapid React/src/main/java/frc/robot/subsystems/OdometrySubsystem.java
@@ -4,5 +4,47 @@
 
 package frc.robot.subsystems;
 
+
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+
 /** Add your docs here. */
-public class OdometrySubsystem {}
+public class OdometrySubsystem extends SubsystemBase{
+    int initialX;
+    int initialY;
+    int robotRX;
+    int robotRY;
+    int robotLX;
+    int robotLY;
+
+
+    public void mapChange(){
+        double angle = Robot.m_driveSubsystem.getAngle();
+        double leftVlo = Robot.m_driveSubsystem.getLeftVelocity();
+        double rightVlo = Robot.m_driveSubsystem.getRightVelocity();
+
+        double leftDistance = leftVlo * .02;
+        double rightDistance = rightVlo * .02;
+        
+        double deltaLX = Math.sin(90-angle)/Math.sin(90)/leftDistance;
+        double deltaLY = Math.sin(angle)/Math.sin(90)/leftDistance;
+
+        double deltaRX = Math.sin(90-angle)/Math.sin(90)/rightDistance;
+        double deltaRY = Math.sin(angle)/Math.sin(90)/rightDistance;
+
+        robotLX += deltaLX;
+        robotLY += deltaLY;
+        robotRX += deltaRX;
+        robotRY += deltaRY;
+
+
+
+        SmartDashboard.putNumber("LeftWheelCoordinateX", robotLX);
+        SmartDashboard.putNumber("LeftWheelCoordinateY", robotLY);
+        SmartDashboard.putNumber("RightWheelCoordinateX", robotRX);
+        SmartDashboard.putNumber("RightWheelCoordinateY", robotRY);
+    }
+
+}
